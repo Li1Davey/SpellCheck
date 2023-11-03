dde85eb10bf6fdd58a4d4e9c867f90f6d2c6f672
Li1Davey <89932997+Li1Davey@users.noreply.github.com>
2023-11-03 17:22:08 -0600

    Update extractData.py

--- extractData.py
+++ extractData.py
@@ -5,17 +5,14 @@ David Sanchez
 def getDictionaryBigrams():
     file = 'wp_2gram.txt'
     fp = open(file, encoding="utf8")
-    limit = 10
     total = 0
     dict_bigrams = []
     for line in fp.readlines():
-        if(limit > 0):
-            x = line.replace('\n', '')
-            a = x.split('\t')
-            total += int(a[0])
-            a[0] = int(a[0])
-            #print(a)
-            dict_bigrams.append
-        limit-=1
+        x = line.replace('\n', '')
+        a = x.split('\t')
+        total += int(a[0])
+        a[0] = int(a[0])
+        #print(a)
+        dict_bigrams.append
     print(total)
     return dict_bigrams, total
