f86f4f7cfaff9ba45cacfb3e1a1d89c785e6c9cc
Li1Davey <89932997+Li1Davey@users.noreply.github.com>
2023-11-03 14:23:27 -0600

    Add files via upload

---
+++ readDocument.py
@@ -0,0 +1,18 @@
+'''
+code from
+https://stackoverflow.com/questions/48869423/how-do-i-copy-the-contents-of-a-word-document
+'''
+from docx import Document
+
+#_Main---------------------------------------------------
+input = Document('Source.docx')
+paragraphs = []
+for para in input.paragraphs:
+    p = para.text
+    paragraphs.append(p)
+
+print(paragraphs)
+output = Document()
+for item in paragraphs:
+    output.add_paragraph(item)
+output.save('New.docx')
