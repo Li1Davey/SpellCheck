5d98dc5ebd39c71510201db060585e870dc54860
cpedroza1 <cpedroza.cp@gmail.com>
2023-11-04 12:39:02 -0600

    Added unigram function

--- extractData.py
+++ extractData.py
@@ -16,3 +16,20 @@ def getDictionaryBigrams():
         dict_bigrams.append
     print(total)
     return dict_bigrams, total
+
+def getDictionaryUnigrams():
+    file = 'wp_1gram.txt'
+    fp = open(file, encoding="utf8")
+    total = 0
+    dict_unigrams = []
+    for line in fp.readlines():
+        x = line.replace('\n', '') 
+        a = x.split('\t')
+        total += int(a[0])
+        a[0] = int(a[0])
+        print(a)
+        dict_unigrams.append
+    print(total)
+    return dict_unigrams, total
+
+
