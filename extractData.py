336955140433521a7eb24b30dc9a8e055954185e
Li1Davey <89932997+Li1Davey@users.noreply.github.com>
2023-11-06 17:23:40 -0700

    Update extractData.py

--- extractData.py
+++ extractData.py
@@ -3,34 +3,31 @@ David Sanchez
 '''
 
 def getDictionaryBigrams():
-    file = 'wp_2gram.txt'
-    fp = open(file, encoding="utf8")
-    total = 0
     dict_bigrams = []
-    for line in fp.readlines():
-        x = line.replace('\n', '')
-        a = x.split('\t')
-        total += int(a[0])
-        a[0] = int(a[0])
-        #print(a)
-        dict_bigrams.append
-    print(total)
-    return dict_bigrams, total
+    i = 0
+    with open("new_wp_2gram.txt", encoding="utf8") as file:
+        for line in file:
+            x = line.replace('\n', '')
+            a = x.split('\t')
+            a[0] = int(a[0])
+            if(len(a)==3):
+                dict_bigrams.append(a)
+            '''
+            if (i % 10000000 == 0):
+                print(i)
+            i+=1
+            '''
+    #print("Finished")
+    return dict_bigrams
 
 def getDictionaryUnigrams():
     file = 'wp_1gram.txt'
     fp = open(file, encoding="utf8")
-    total = 0
     dict_unigrams = {}
     for line in fp.readlines():
         x = line.replace('\n', '') 
         a = x.split('\t')
-        total += int(a[0])
         count = int(a[0])
         word = a[1]
         dict_unigrams[word] = count/1548800152
-    return dict_unigrams, total
-
-
-
-
+    return dict_unigrams
