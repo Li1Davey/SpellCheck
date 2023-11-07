fa1edf9ff396cdb2e7313d1edb443f7317ecb5d5
Li1Davey <89932997+Li1Davey@users.noreply.github.com>
2023-11-06 20:30:26 -0700

    Update extractData.py

--- extractData.py
+++ extractData.py
@@ -1,23 +1,24 @@
 '''
-David Sanchez
+Program by Cristian Pedroza & David Sanchez
 '''
 
 def getDictionaryBigrams():
     dict_bigrams = []
-    i = 0
+    #i = 0
     with open("new_wp_2gram.txt", encoding="utf8") as file:
         for line in file:
             x = line.replace('\n', '')
             a = x.split('\t')
             a[0] = int(a[0])
-            if(len(a)==3):
-                dict_bigrams.append(a)
+            dict_bigrams.append(a)
             '''
+            #Used to keep track of how many lines are already extracted
             if (i % 10000000 == 0):
                 print(i)
             i+=1
             '''
-    #print("Finished")
+    #print("Bigram Dictionary Results (First 10 lines)")
+    #print(dict_bigrams[:10])
     return dict_bigrams
 
 def getDictionaryUnigrams():
@@ -30,4 +31,6 @@ def getDictionaryUnigrams():
         count = int(a[0])
         word = a[1]
         dict_unigrams[word] = count/1548800152
+    #print("Unigram Dictionary Results (First 10 lines)")
+    #print(dict(list(dict_unigrams.items())[0: 10]))
     return dict_unigrams
